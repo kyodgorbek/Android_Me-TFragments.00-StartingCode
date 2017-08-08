5d69328403bcf0930468a961190c8e3452ed948f
kyodgorbek <kyodgorbek@gmail.com>
2017-08-08 15:13:27 +0200

    I have implemented MasterListFragment

---
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -0,0 +1,60 @@
+package com.example.android.android_me.ui;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+/**
+ * Created by yodgorbekkomilov on 8/7/17.
+ */
+
+public class MasterListFragment extends Fragment {
+
+
+   OnImageClickListener mCallBack;
+
+    public interface OnImageClickListener{
+        void onImageSelected(int position);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+
+
+    }
+
+    public MasterListFragment() {
+    }
+
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        final View rootView = inflater.inflate(R.layout.activity_list, container, false);
+
+        GridView gridView = (GridView)rootView.findViewById(R.id.images_grid_view);
+
+        MasterListAdapter mAdapter = new MasterListAdapter(getContext(), AndroidImageAssets.getAll());
+        gridView.setAdapter(mAdapter);
+
+        return rootView;
+
+
+    }
+}
